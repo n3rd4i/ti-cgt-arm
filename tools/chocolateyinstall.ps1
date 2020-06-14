00fd6a431dc69d419e53400d27d5ac21851afb24
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-14 21:24:34 +0200

    refactored info

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,17 +1,15 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://software-dl.ti.com/codegen/esd/cgt_public_sw/TMS470/18.1.4.LTS/ti_cgt_tms470_18.1.4.LTS_windows_installer.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' #only one of these: exe, msi, msu
-  url           = $url
-  softwareName  = 'ti_cgt_tms470*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType      = 'exe'
+  url           = 'http://software-dl.ti.com/codegen/esd/cgt_public_sw/TMS470/18.1.4.LTS/ti_cgt_tms470_18.1.4.LTS_windows_installer.exe'
+  softwareName  = 'ti_cgt_tms470*'
   checksum      = 'F55B7D78174F09A51C6A4B181EA515CA'
-  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
+  checksumType  = 'md5'
   silentArgs    = '--prefix "c:\ti\ccs901\ccs\tools\compiler" --unattendedmodeui none --mode unattended'
   validExitCodes= @(0, 3010, 1641)
 }
-
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
+Install-ChocolateyPackage @packageArgs
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,19 +1,19 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'ti_cgt_tms470*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  fileType      = 'exe' #only one of these: MSI or EXE (ignore MSU for now)
+  softwareName  = 'ti_cgt_tms470*'
+  fileType      = 'exe'
   silentArgs    = "--unattendedmodeui none --mode unattended"
-  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
 }
 
 $uninstalled = $false
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
-  $key | % { 
-    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
-    
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+
     if ($packageArgs['fileType'] -eq 'MSI') {
       $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
       $packageArgs['file'] = ''
